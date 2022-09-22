99ebf392afb25b50f1d818a09c643550c6dee90a
Nancy Jain <nancyjain0631@gmail.com>
2022-09-22 20:53:54 +0530

    Initial Commit

---
+++ GitIUnderstanding/GitIUnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitIUnderstandingApp.swift
+//  GitIUnderstanding
+//
+//  Created by Nancy Jain on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GitIUnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
