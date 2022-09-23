1b7cc80e5523672614227d04bac9cc7dcb11d45f
Nancy Jain <nancyjain0631@gmail.com>
2022-09-23 11:02:15 +0530

    change color to pink

--- GitIUnderstanding/ContentView.swift
+++ GitIUnderstanding/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             NavigationLink(destination: ModalView()) {
                 Text("Go next")
             }
+            .foregroundColor(.pink)
             .navigationTitle("Xcode & Git")
         }
         
